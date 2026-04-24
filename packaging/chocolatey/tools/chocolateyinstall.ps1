85a879833f1ee3eda47304299f68e43e5e2de84c
mulhamna <mulhamna@gmail.com>
2026-04-24 15:34:12 +0000

    chore(packaging): add chocolatey release lane

---
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'jirac'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$url64 = 'https://github.com/mulhamna/jira-commands/releases/download/v0.16.5/jirac-windows-x86_64.zip'
+$checksum64 = '__SHA256_WINDOWS_X86__'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'zip'
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$exePath = Get-ChildItem -Path $toolsDir -Filter 'jirac*.exe' | Select-Object -First 1
+if (-not $exePath) {
+  throw 'Expected jirac.exe not found after install'
+}
+
+Install-BinFile -Name 'jirac' -Path $exePath.FullName

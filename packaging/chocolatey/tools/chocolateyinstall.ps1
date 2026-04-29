2cca485ca9419e813dda8ccbbfb16453c0f7f47a
mulhamna <97831705+mulhamna@users.noreply.github.com>
2026-04-29 16:00:23 +0000

    chore(packaging): refresh release docs and manifests for v0.24.4

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'jirac'
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$url64 = 'https://github.com/mulhamna/jira-commands/releases/download/v0.23.0/jirac-windows-x86_64.zip'
-$checksum64 = '9c3c4067e6f7ef4ba2a3b66ae37a3cbaa892ad9deb2196a5436decfbeb1fcf0b'
+$url64 = 'https://github.com/mulhamna/jira-commands/releases/download/v0.24.4/jirac-windows-x86_64.zip'
+$checksum64 = '46e865312256e8c6bb4188c6ae9be6d283aa091f4db3dc037f419a129bcfb3c6'
 
 $packageArgs = @{
   packageName    = $packageName
